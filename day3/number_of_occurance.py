4b3cd53b99f75de13be00148c34825f4fcc2ec6c
Shahrojali321 <102039200+Shahrojali321@users.noreply.github.com>
2022-06-07 21:21:12 +0530

    Add files via upload

--- day3/number_of_occurance.py
+++ day3/number_of_occurance.py
@@ -1,19 +1,18 @@
-"""Defination: Program that Reads a Text File and Counts the Number of Times a Certain Letter Appears in the Text File"""
+"""Defination:Program that Reads a Text File and Counts Certain Letter Appears in the Text File"""
 def letter_count(file1, letter):
     """For count the letter in file"""
     file = open(file1, 'r')
-    lst=[]
     for line in file:
         line=line.strip()
         line=line.lower().replace(".","")
         word=line.split()
     print(word)
     count=0
-    for i in word:
-        if i==letter:
+    for count_letter in word:
+        if count_letter==letter:
             count=count+1
     return count
- 
-letter=input("Enter the letter which you want to count: ")
-result=letter_count('d:/pratice/pramods_code/new_module/day3/file1.txt', letter)
-print(f"Number of times the letter found in file is: {result}")
+if __name__=='__main__':
+    LETTER=input("Enter the letter which you want to count: ")
+    RESULT=letter_count('d:/pratice/pramods_code/new_module/day3/file1.txt', LETTER)
+    print(f"Number of times the letter found in file is: {RESULT}")
--- day3/smallest_lasgest_in_tuple.py
+++ day3/smallest_lasgest_in_tuple.py
@@ -1,19 +1,21 @@
 """Defination:Program to Find the Largest and Smallest Item in a Tuple"""
-lst=[]
-def largest_smallest(Tuple):
-    tup_largest = Tuple[0]
-    tup_smallest=Tuple[0]
-    for i in Tuple:
-        if(tup_largest < i):
-            tup_largest = i
-        if(tup_smallest > i):
-            tup_smallest = i
+def largest_smallest(tuple1):
+    """Algorithm for largest and smallest in tuple"""
+    tup_largest = tuple1[0]
+    tup_smallest=tuple1[0]
+    for char in tuple1:
+        if(tup_largest < char):
+            tup_largest = char
+        if(tup_smallest > char):
+            tup_smallest = char
     print(f"The lasgest number in tuple is: {tup_largest}")
     print(f"The smallest number in touple is : {tup_smallest}")
-n=int(input("enter the length of tuple: "))
-for i in range(0,n):
-    element=input("Enter the element of a tuple: ")
-    lst.append(element)
-Tuple=tuple(lst)
-print(Tuple)
-result=largest_smallest(Tuple)
+if __name__=='__main__':
+    lst=[]
+    list_length=int(input("enter the length of tuple: "))
+    for i in range(0,list_length):
+        element=input("Enter the element of a tuple: ")
+        lst.append(element)
+    TUPLE=tuple(lst)
+    print(TUPLE)
+    largest_smallest(TUPLE)
