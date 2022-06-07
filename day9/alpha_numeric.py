42deb1f1f048c0d52260774ae1d49424d2668a81
Shahrojali321 <102039200+Shahrojali321@users.noreply.github.com>
2022-06-07 21:27:49 +0530

    Add files via upload

--- day9/alpha_numeric.py
+++ day9/alpha_numeric.py
@@ -1,6 +1,5 @@
 """Defination: Program to accept string ending with alphanumeric character"""
 import re
-REGEX_EXPRESSION = '[a-zA-z0-9]$'
 def check_string(my_string):
     """algorithm to accept the string ending with alphanumeric character"""
     if(re.search(REGEX_EXPRESSION, my_string)):
@@ -8,6 +7,7 @@ def check_string(my_string):
     else:
         print("The string doesnot end with alphanumeric character")
 if __name__=='__main__':
+    REGEX_EXPRESSION = '[a-zA-z0-9]$'
     TEST_STRING=input("Enter the string : ")
     print(f"Your string is: \n {TEST_STRING}")
     check_string(TEST_STRING)
